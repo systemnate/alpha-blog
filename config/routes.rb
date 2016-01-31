4cd429622a787bd259eb35fd05a92b7b4b3a77a3
Nate Dalo <nate.dalo@yahoo.com>
2016-01-31 10:54:11 -0600

    Create sessions controller for logging in/out

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
 end
